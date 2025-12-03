7226093f541a4d41a7505eaeec6f5acf3035758b
YoganRX <johan8859@live.se>
2025-12-03 18:42:31 +0100

    Uppgift 5
    Skapa funktion för eventlyssnare till inputfält

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ const textFields = document.getElementsByClassName("textfield");
 const button = document.getElementById("btnRemove");
 
 const divElement = document.getElementsByTagName("div");
+
+function handleInput(e) {
+    console.log(e.target);
+
+    const inputName = e.target.name;
+
+    if (inputName === "content") {
+        divElement.innerHTML = e.target.value;
+    }
+}
